5cb8041f203a65c93bf4ee3084ffd25dd78e7acd
Siddhartha Maharjan <94985027+Theamage@users.noreply.github.com>
2024-07-26 20:17:00 +0930

    Initial commit of number guessing game

---
+++ guessing_game.py
@@ -0,0 +1,27 @@
+import random
+
+def number_guessing_game():
+    secret_number = random.randint(1, 20)
+    attempts = 0
+
+    print("I'm thinking of a number between 1 and 20.")
+
+    while True:
+        try:
+            guess = int(input("Take a guess: "))
+        except ValueError:
+            print("Please enter a number.")
+            continue
+
+        attempts += 1
+
+        if guess < secret_number:
+            print("Your guess is too low.")
+        elif guess > secret_number:
+            print("Your guess is too high.")
+        else:
+            print(f"Good job! You guessed my number in {attempts} attempts!")
+            break
+
+if __name__ == "__main__":
+    number_guessing_game()
