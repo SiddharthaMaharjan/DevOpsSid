8a7a5a46b04ddb034447e206c872a8bd06871457
Siddhartha Maharjan <94985027+SiddharthaMaharjan@users.noreply.github.com>
2024-08-19 14:23:11 +0930

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,17 +1,21 @@
+
 import random
-import sys
 
 def number_guessing_game():
+
+
     secret_number = random.randint(1, 30)
-    attempts = 0
+    attempts = 5
 
-    print("I'm thinking of a number between 1 and 30.")
 
-    while attempts < 5:
+
+    print("I'm thinking of a number between 1 and 20.")
+
+    while True:
         try:
             guess = int(input("Take a guess: "))
         except ValueError:
-            print("Please enter a valid number.")
+            print("Please enter a number.")
             continue
 
         attempts += 1
@@ -21,11 +25,8 @@ def number_guessing_game():
         elif guess > secret_number:
             print("Your guess is too high.")
         else:
-            print(f"Congratulations! You guessed my number in {attempts} attempts!")
-            sys.exit(0)
-
-    print(f"Sorry, you've used all attempts. The number was {secret_number}.")
-    sys.exit(1)
+            print(f"Congratulations You! You guessed my number in {attempts} attempts!")
+            break
 
 if __name__ == "__main__":
     number_guessing_game()
