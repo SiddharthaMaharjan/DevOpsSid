ad0124a232482da165e923ac7924449dc9237235
Siddhartha Maharjan <94985027+SiddharthaMaharjan@users.noreply.github.com>
2024-08-20 14:20:26 +0930

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -4,7 +4,7 @@ def number_guessing_game():
     secret_number = random.randint(1, 30)
     attempts = 5
 
-    print("I'm thinking of number between 1 and 20.")
+    print("I'm thinking of a number between 1 and 20.")
     for _ in range(attempts):  # Limit the number of attempts
         try:
             guess = int(input("Take  guess: "))
